9f1db510a1c295285220bb5fbe78d34e22b368cc
huyqueng <huyqueng@gmail.com>
2024-12-24 15:59:35 +0700

    add to the cart

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose')
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, { timestamps: true })
-
-const cartItemsSchema = new mongoose.Schema({
-  cartId: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart', required: true },
-  variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductVariant', required: true },
-  quantity: { type: Number, required: true, default: 1, min: 1 },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  sessionId: { type: String, required: true },
+  items: [
+    {
+      variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductVariant', required: true },
+      quantity: { type: Number, required: true, default: 1, min: 1 },
+      price: { type: Number, required: true, default: 0 }
+    }
+  ],
+  totalPrice: { type: Number , required: true}
 }, { timestamps: true })
 
 const Cart = mongoose.model('Cart', cartSchema)
-const CartItems = mongoose.model('ProductVariant', cartItemsSchema)
 
-module.exports = {
-  Cart,
-  CartItems
-}
+module.exports = Cart
--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true, maxlegth: 30, unique: true },
   products: [
-    {
-      type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true
-    }
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }
   ]
 },{ timestamps: true })
 
