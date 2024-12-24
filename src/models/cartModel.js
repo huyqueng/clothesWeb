cc97db321254d9c1822a10ce702f13be2ccc112b
huyqueng <huyqueng@gmail.com>
2024-12-25 01:27:27 +0700

    update add item to card

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const cartSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-  sessionId: { type: String, required: true },
   items: [
     {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
       variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductVariant', required: true },
       quantity: { type: Number, required: true, default: 1, min: 1 },
       price: { type: Number, required: true, default: 0 }
